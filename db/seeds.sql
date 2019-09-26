aba4d5f704b37510181161b9c0f6ed2f757ba51b
Cesar Silva <silvacde@gmail.com>
2019-09-25 21:07:24 -0700

    created db schema and seeds file with successful creation of table

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT  INTO burgers (burger_name) VALUES ('Double-Bacon');
+INSERT  INTO burgers (burger_name) VALUES ('Vegetarian Patty with Cheese');
+INSERT  INTO burgers (burger_name) VALUES ('Fish Fillet');
+INSERT  INTO burgers (burger_name) VALUES ('Single Patty');
