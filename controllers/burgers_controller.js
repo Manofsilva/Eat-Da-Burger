8ceab53a796aac13fe11571ce6708bfc85b13611
Cesar Silva <silvacde@gmail.com>
2019-09-28 11:44:45 -0700

    collecting data properly after setting up selectAll route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,13 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get('/', function(req, res) {
-    res.render('index');
-})
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+});
+});
 
 module.exports = router;
