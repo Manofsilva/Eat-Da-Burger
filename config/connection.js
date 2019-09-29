e04b0b64483e635e079f452302ca3024560a3810
Cesar Silva <silvacde@gmail.com>
2019-09-28 22:19:07 -0700

    added jawsDB code

--- config/connection.js
+++ config/connection.js
@@ -10,14 +10,7 @@ var connection = mysql.createConnection({
   });
 
   if(process.env.NODE_ENV === "production"){
-    connection = mysql.createConnection({
-      host: process.env.JAWSHOST,
-      user: process.env.JAWSUSER,
-      port: process.env.JAWSPORT,
-      password: process.env.JAWSPWD,
-      database: process.env.JAWSDB,
-      use_env_variable: process.env.JAWSDB_URL
-    });
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
   }
   
   // Make connection.
