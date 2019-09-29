03ac1f6b984b9094cbd20613a5c20fd61701b3ec
Cesar Silva <silvacde@gmail.com>
2019-09-28 17:53:36 -0700

    updated controllers file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,7 +21,7 @@ router.post("/api/burgers", function(req, res) {
     ], [
         req.body.burger_name, req.body.devoured
     ], function(result) {
-        res.redirect("/");
+        res.json({ id: result.insertId});
     });
 });
 
